4349c4348b65cfd4bc87fbb627932f55cf1cd807
Sam <samshum90@gmail.com>
2020-07-17 23:40:59 +0100

    added serializer

---
+++ simplecrm/crmapp/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Account, Activity, ActivityStatus, Contact, ContactSource, ContactStatus
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = "__all__"
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = "__all__"
+
+class ContactSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = ActivityStatus
+        fields = "__all__"
+
+class ContactSourceSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = ContactSource
+        fields = "__all__"
+
+class ContactStatusSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = ContactStatus
+        fields = "__all__"
