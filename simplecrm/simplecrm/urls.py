c46abb04897337ecf83fa95f6cc49ad7a826e9f5
Sam <samshum90@gmail.com>
2020-07-18 00:17:12 +0100

    added API URLS

--- simplecrm/simplecrm/urls.py
+++ simplecrm/simplecrm/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crmapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'accounts', views.AccountAPIView.as_view(), name='account-list'),
+    path(r'contacts', views.ContactAPIView.as_view(), name='contact-list'),
+    path(r'activities', views.ActivityAPIView.as_view(), name='activity-list'),
+    path(r'activitystatuses', views.ActivityStatusAPIView.as_view(), name='activity-status-list'),
+    path(r'contactsources', views.ContactSourceAPIView.as_view(), name='contact-source-list'),
+    path(r'contactstatuses', views.ContactStatusAPIView.as_view(), name='contact-status-list')
 ]
